7eeed13f8db0c0d18af2075875f7e168ebbbb780
Joel Zeng <joelzeng@hotmail.com>
2015-11-14 17:34:55 -0500

    first commit

---
+++ gesturescript.lua
@@ -0,0 +1,30 @@
+scriptId = 'com.thalmic.examples.gesturescript'
+scriptTitle = "Gesture script"
+scriptDetailsUrl = ""
+
+--file = io.open("words.txt", "a")
+--io.output(file)
+
+--function hello()
+--myo.debug("FIST PRINTED")
+--end
+
+function onPoseEdge(pose, edge)
+    myo.debug("onPoseEdge: " .. pose .. ", " .. edge)
+end
+
+function onPeriodic()
+end
+
+function onForegroundWindowChange(app, title)
+    --myo.debug("onForegroundWindowChange: " .. app .. ", " .. title)
+    return true
+end
+
+function activeAppName()
+    return "Output Everything"
+end
+
+function onActiveChange(isActive)
+    myo.debug("onActiveChange I'm joel")
+end
