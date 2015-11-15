be614abeca5c62e77b943af2dc9a3ee281d2c6c6
Joel Zeng <joelzeng@hotmail.com>
2015-11-15 03:07:37 -0500

    lol

--- gesturescript.lua
+++ gesturescript.lua
@@ -16,9 +16,13 @@ function onPoseEdge(pose, edge)
 		myo.debug("What time is it?")
 		myo.keyboard("h","press")
 		myo.keyboard("return","press")
+		myo.keyboard("h","press")
+		myo.keyboard("return","press")
 	end
 	if edge == "on" and pose == "waveIn" and pitch < .5 then
 		myo.debug("Thank You")
+		myo.keyboard("n","press")
+		myo.keyboard("return","press")
 		myo.keyboard("n","press")
 		myo.keyboard("return","press")
 	end
@@ -26,9 +30,13 @@ function onPoseEdge(pose, edge)
 		myo.debug("Hello")
 		myo.keyboard("a","press")
 		myo.keyboard("return","press")
+		myo.keyboard("a","press")
+		myo.keyboard("return","press")
 	end
 	if edge == "on" and pose == "fist" and pitch > .5 then
 		myo.debug("Where is the washroom?")
+		myo.keyboard("i","press")
+		myo.keyboard("return","press")
 		myo.keyboard("i","press")
 		myo.keyboard("return","press")
 	end
@@ -36,9 +44,13 @@ function onPoseEdge(pose, edge)
 		myo.debug("where is the closest restaurant?")
 		myo.keyboard("c","press")
 		myo.keyboard("return","press")
+		myo.keyboard("c","press")
+		myo.keyboard("return","press")
 	end
 	if edge == "on" and pose == "waveOut" and pitch > .5 and yaw > -1.5 then
 		myo.debug("Can you help find _")
+		myo.keyboard("k","press")
+		myo.keyboard("return","press")
 		myo.keyboard("k","press")
 		myo.keyboard("return","press")
 	end
@@ -46,9 +58,13 @@ function onPoseEdge(pose, edge)
 		myo.debug("Where is the closest bus station?")
 		myo.keyboard("f","press")
 		myo.keyboard("return","press")
+		myo.keyboard("f","press")
+		myo.keyboard("return","press")
 	end
 	if edge == "on" and pose == "fingersSpread" and pitch > .5 then
 		myo.debug("I need help, call 9-1-1")
+		myo.keyboard("d","press")
+		myo.keyboard("return","press")
 		myo.keyboard("d","press")
 		myo.keyboard("return","press")
 	end
@@ -56,9 +72,13 @@ function onPoseEdge(pose, edge)
 		myo.debug("Goodbye")
 		myo.keyboard("b","press")
 		myo.keyboard("return","press")
+		myo.keyboard("b","press")
+		myo.keyboard("return","press")
 	end
 	if edge == "on" and pose == "doubleTap" and pitch > .5 then
 		myo.debug("I love Digiflare")
+		myo.keyboard("p","press")
+		myo.keyboard("return","press")
 		myo.keyboard("p","press")
 		myo.keyboard("return","press")
 	end
