765d457233800b99aaa55f34c438eba95f013f7c
Andrew Lee <andrewjplee586@gmail.com>
2015-11-14 23:31:15 -0500

    update

--- gesturescript.lua
+++ gesturescript.lua
@@ -1,40 +1,64 @@
 scriptId = 'com.thalmic.examples.gesturescript'
-scriptTitle = "Gesture script"
+scriptTitle = "debug"
 scriptDetailsUrl = ""
 
 myo.setLockingPolicy("none")
+
 
 function onPoseEdge(pose, edge)
     --myo.debug("onPoseEdge: " .. pose .. ", " .. edge)
     pitch = myo.getPitch()
     roll = myo.getRoll()
+	yaw = myo.getYaw()
+	
     pi = math.pi
-    if edge == "on" then
-    		if pose == "waveOut" and pitch > pi/6 then
-    			myo.debug("upHELLO")
-    		elseif pose == "waveOut" and pitch < pi/6 then
-    			myo.debug("downHELLO")
-    		elseif pose == "fingersSpread" and pitch < pi/6 and roll < -pi/2 then
-    			myo.debug("How much?")
-    		elseif pose == "waveIn" and pitch > pi/6 then
-    			myo.debug("greater than 45 detected")
-    				myo.debug("FOOD")
-    		elseif pose == "fist" and pitch < pi/6 then
-    			myo.debug("fist")
-    		elseif pose == "doubleTap" and pitch > pi/6 then
-    			myo.debug("dt")
-    	    end
-    	
-    end
+	if edge == "on" and pose == "fist" and pitch < .5 then
+		myo.debug("time")
+		myo.keyboard("h","press")
+	end
+	if edge == "on" and pose == "waveIn" and pitch < .5 then
+		myo.debug("Thank You")
+		myo.keyboard("n","press")
+	end
+	if edge == "on" and pose == "waveOut" and pitch < .5 then
+		myo.debug("Hello")
+		myo.keyboard("a","press")
+	end
+	if edge == "on" and pose == "fist" and pitch > .5 then
+		myo.debug("washroom")
+		myo.keyboard("i","press")
+	end
+	if edge == "on" and pose == "waveIn" and pitch > .5 then
+		myo.debug("food")
+		myo.keyboard("c","press")
+	end
+	if edge == "on" and pose == "waveOut" and pitch > .5 then
+		myo.debug("Do You Speak English")
+		myo.keyboard("a","press")
+	end
+	if edge == "on" and pose == "fingerSpread" and pitch < .5 then
+		myo.debug("How do I get?")
+		myo.keyboard("k","press")
+	end
+	if edge == "on" and pose == "fingerSpread" and pitch > .5 then
+		myo.debug("I need Help")
+		myo.keyboard("d","press")
+	end
+	if edge == "on" and pose == "doubleTap" and pitch < .5 then
+		myo.debug("bye")
+		myo.keyboard("b","press")
+	end
+	if edge == "on" and pose == "doubleTap" and pitch > .5 then
+		myo.debug("<3 digiflare")
+		myo.keyboard("p","press")
+	end
 end
 
 
 --waveIn, waveOut, fist, doubleTap, fingersSpread, rest and unknown. rest
 
 function onPeriodic()
-	--myo.debug("roll is: ".. myo.getRoll())
-	--myo.debug("pitch is: ".. myo.getPitch())
-	--myo.debug("yaw is: ".. myo.getYaw())
+	--myo.debug(myo.getPitch())
 end
 
 function onForegroundWindowChange(app, title)
@@ -49,4 +73,3 @@ end
 --function onActiveChange(isActive)
     --myo.debug("onActiveChange")
 --end
-
