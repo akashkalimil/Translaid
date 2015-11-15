eb3ba4ec970c3a1b110e32c9ce63f8400ee3b52b
Joel Zeng <joelzeng@hotmail.com>
2015-11-15 02:30:00 -0500

    fix

--- gesturescript.lua
+++ gesturescript.lua
@@ -15,42 +15,52 @@ function onPoseEdge(pose, edge)
 	if edge == "on" and pose == "fist" and pitch < .5 then
 		myo.debug("What time is it?")
 		myo.keyboard("h","press")
+		myo.keyboard("return","press")
 	end
 	if edge == "on" and pose == "waveIn" and pitch < .5 then
 		myo.debug("Thank You")
 		myo.keyboard("n","press")
+		myo.keyboard("return","press")
 	end
 	if edge == "on" and pose == "waveOut" and pitch < .5 and yaw > -1.25 then
 		myo.debug("Hello")
 		myo.keyboard("a","press")
+		myo.keyboard("return","press")
 	end
 	if edge == "on" and pose == "fist" and pitch > .5 then
 		myo.debug("Where is the washroom?")
 		myo.keyboard("i","press")
+		myo.keyboard("return","press")
 	end
 	if edge == "on" and pose == "waveIn" and pitch > .5 then
 		myo.debug("where is the closest restaurant?")
 		myo.keyboard("c","press")
+		myo.keyboard("return","press")
 	end
 	if edge == "on" and pose == "waveOut" and pitch > .5 and yaw > -1.5 then
 		myo.debug("Can you help find _")
 		myo.keyboard("k","press")
+		myo.keyboard("return","press")
 	end
 	if edge == "on" and pose == "fingersSpread" and pitch < .5 then
 		myo.debug("Where is the closest bus station?")
 		myo.keyboard("f","press")
+		myo.keyboard("return","press")
 	end
 	if edge == "on" and pose == "fingersSpread" and pitch > .5 then
 		myo.debug("I need help, call 9-1-1")
 		myo.keyboard("d","press")
+		myo.keyboard("return","press")
 	end
 	if edge == "on" and pose == "doubleTap" and pitch < .5 then
 		myo.debug("Goodbye")
 		myo.keyboard("b","press")
+		myo.keyboard("return","press")
 	end
 	if edge == "on" and pose == "doubleTap" and pitch > .5 then
 		myo.debug("I love Digiflare")
 		myo.keyboard("p","press")
+		myo.keyboard("return","press")
 	end
 end
 
@@ -60,6 +70,7 @@ end
 function onPeriodic()
 	--myo.debug(myo.getPitch())
 	--myo.debug(myo.getYaw())
+	--myo.debug(myo.getRoll())
 end
 
 function onForegroundWindowChange(app, title)
