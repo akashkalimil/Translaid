9b47741a68968cc08f72ab4941857c2f5896869b
Andrew Lee <andrewjplee586@gmail.com>
2015-11-15 00:25:53 -0500

    fixed phrases

--- gesturescript.lua
+++ gesturescript.lua
@@ -13,7 +13,7 @@ function onPoseEdge(pose, edge)
 	
     pi = math.pi
 	if edge == "on" and pose == "fist" and pitch < .5 then
-		myo.debug("time")
+		myo.debug("What time is it?")
 		myo.keyboard("h","press")
 	end
 	if edge == "on" and pose == "waveIn" and pitch < .5 then
@@ -25,31 +25,31 @@ function onPoseEdge(pose, edge)
 		myo.keyboard("a","press")
 	end
 	if edge == "on" and pose == "fist" and pitch > .5 then
-		myo.debug("washroom")
+		myo.debug("Where is the washroom?")
 		myo.keyboard("i","press")
 	end
 	if edge == "on" and pose == "waveIn" and pitch > .5 then
-		myo.debug("food")
+		myo.debug("where is the closest restaurant?")
 		myo.keyboard("c","press")
 	end
 	if edge == "on" and pose == "waveOut" and pitch > .5 then
-		myo.debug("Do You Speak English")
-		myo.keyboard("a","press")
+		myo.debug("Can you help find _")
+		myo.keyboard("k","press")
 	end
 	if edge == "on" and pose == "fingersSpread" and pitch < .5 then
 		myo.debug("How do I get?")
 		myo.keyboard("k","press")
 	end
 	if edge == "on" and pose == "fingersSpread" and pitch > .5 then
-		myo.debug("I need Help")
-		myo.keyboard("d","press")
-	end
+		myo.debug("Where is the closest bus station?")
+		myo.keyboard("f","press")
+	enda
 	if edge == "on" and pose == "doubleTap" and pitch < .5 then
-		myo.debug("bye")
+		myo.debug("Goodbye")
 		myo.keyboard("b","press")
 	end
 	if edge == "on" and pose == "doubleTap" and pitch > .5 then
-		myo.debug("<3 digiflare")
+		myo.debug("I love Digiflare")
 		myo.keyboard("p","press")
 	end
 end
